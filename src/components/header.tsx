dff360f89f8d751223b91ae3e04e33487c1bd654
Eric Förster <efoerster@users.noreply.github.com>
2019-01-17 11:57:57 +0100

    Make navigation bar responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,6 +5,7 @@ import {
   Container,
   Navbar,
   NavbarBrand,
+  NavbarBurger,
   NavbarEnd,
   NavbarItem,
   NavbarMenu,
@@ -14,30 +15,55 @@ import React from 'react';
 
 import Logo from '../images/logo.svg';
 
-export const Header: React.FunctionComponent = () => {
+const NavbarGithubItem = ({ hideOnDesktop = false }) => {
+  const isHidden = hideOnDesktop ? 'desktop' : undefined;
   return (
-    <Navbar className="is-white has-shadow">
-      <Container>
-        <NavbarBrand>
-          <NavbarItem href="/">
-            <img src={Logo} alt="TexLab" />
-          </NavbarItem>
-        </NavbarBrand>
-        <NavbarMenu>
-          <NavbarStart>
-            <NavbarItem href="/">Home</NavbarItem>
-            <NavbarItem href="/docs">Docs</NavbarItem>
-          </NavbarStart>
-          <NavbarEnd>
-            <NavbarItem href="https://github.com/efoerster/texlab">
-              <FontAwesomeIcon icon={faGithub} size="lg" />
-            </NavbarItem>
-            <NavbarItem>
-              <Button isColor="primary">Download</Button>
-            </NavbarItem>
-          </NavbarEnd>
-        </NavbarMenu>
-      </Container>
-    </Navbar>
+    <NavbarItem href="https://github.com/efoerster/texlab" isHidden={isHidden}>
+      <FontAwesomeIcon icon={faGithub} size="lg" />
+    </NavbarItem>
   );
 };
+
+interface HeaderState {
+  isCollapsed: boolean;
+}
+
+export class Header extends React.Component<{}, HeaderState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { isCollapsed: true };
+  }
+
+  public render() {
+    const isActive = !this.state.isCollapsed;
+    return (
+      <Navbar className="is-white has-shadow">
+        <Container>
+          <NavbarBrand>
+            <NavbarItem href="/">
+              <img src={Logo} alt="TexLab" />
+            </NavbarItem>
+            <NavbarGithubItem hideOnDesktop={true} />
+            <NavbarBurger isActive={isActive} onClick={this.handleClick} />
+          </NavbarBrand>
+          <NavbarMenu isActive={isActive} onClick={this.handleClick}>
+            <NavbarStart>
+              <NavbarItem href="/">Home</NavbarItem>
+              <NavbarItem href="/docs">Docs</NavbarItem>
+            </NavbarStart>
+            <NavbarEnd isHidden="touch">
+              <NavbarGithubItem />
+              <NavbarItem>
+                <Button isColor="primary">Download</Button>
+              </NavbarItem>
+            </NavbarEnd>
+          </NavbarMenu>
+        </Container>
+      </Navbar>
+    );
+  }
+
+  private handleClick = () => {
+    this.setState(state => ({ isCollapsed: !state.isCollapsed }));
+  };
+}

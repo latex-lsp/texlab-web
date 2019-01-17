1e6875a35bc644de337fb016af3dcca5cb39dc4a
Eric Förster <efoerster@users.noreply.github.com>
2019-01-17 12:34:19 +0100

    Improve 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,27 @@
+import { Button, Container, Content, Section, Title } from 'bloomer';
 import React from 'react';
 import { Layout } from '../components/layout';
 import { SEO } from '../components/seo';
 
-const NotFoundPage: React.FunctionComponent = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+const NotFoundPage: React.FunctionComponent = () => {
+  const title = 'Page Not Found';
+  return (
+    <Layout>
+      <SEO title={title} />
+      <Container>
+        <Section>
+          <Title>{title}</Title>
+          <Content>
+            The requested page does not exist. Please go back to the home page
+            by using the button below.
+          </Content>
+          <Button isColor="primary" href="/">
+            Take me back
+          </Button>
+        </Section>
+      </Container>
+    </Layout>
+  );
+};
 
 export default NotFoundPage;

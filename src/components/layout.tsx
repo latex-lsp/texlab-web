c940ac90be235188c59f6a157c0e14ce22943cc2
Eric Förster <efoerster@users.noreply.github.com>
2019-02-16 11:34:19 +0100

    Improve color scheme

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,7 +11,7 @@ export const Layout: React.FC = ({ children }) => {
     <>
       <Header />
       {children}
-      <Footer className="has-background-dark">
+      <Footer className="has-background-light">
         <Container>
           <Content>
             <p>

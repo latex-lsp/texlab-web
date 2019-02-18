3e57c29de88edcd9947811db7e2baec7b1ac4c8f
Eric Förster <efoerster@users.noreply.github.com>
2019-02-18 18:40:33 +0100

    Add offline functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,8 +34,6 @@ module.exports = {
       },
     },
     'gatsby-plugin-catch-links',
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 };

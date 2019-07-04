03cac8cf47171fd211d4031d8179f092ec8ffe2c
Patrick Förster <pfoerster@users.noreply.github.com>
2019-07-04 15:09:20 +0200

    Update feature table

--- src/data/features.ts
+++ src/data/features.ts
@@ -52,8 +52,8 @@ export const SERVER_FEATURES: FeatureTableEntry[] = [
   },
   {
     name: 'Document symbols',
-    latex: 'implemented',
-    bibtex: 'implemented',
+    latex: 'not-implemented',
+    bibtex: 'not-implemented',
   },
   {
     name: 'Code action',
